31aa076ee091016cea05e974baaa694e1131a0aa
Alexander Kel <1906.kas@gmail.com>
2016-10-14 18:42:59 +0300

    Main class fix

---
+++ src/main/java/ru/itis/DatabaseMonitoringApplication.java
@@ -0,0 +1,12 @@
+package ru.itis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseMonitoringApplication.class, args);
+	}
+}
