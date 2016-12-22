f64b70e697ecf3fdbcaa47505624e8fdeea6fec7
Булат Усманов <be.constructive.site.admin@gmail.com>
2016-12-22 23:53:03 +0300

    added separate signs

--- src/main/resources/statements/initial_script.sql
+++ src/main/resources/statements/initial_script.sql
@@ -1,30 +1,43 @@
 CREATE SCHEMA monitoring_schema;
 
+CREATE TABLE monitoring_schema.query (
+  id        INT PRIMARY KEY NOT NULL,
+  name      VARCHAR(200)    NOT NULL,
+  statement VARCHAR(1000)   NOT NULL
+);
 
-CREATE TABLE monitoring_schema.query(
-   id INT PRIMARY KEY     NOT NULL,
-   name           VARCHAR(200)    NOT NULL,
-   statement      VARCHAR(1000)     NOT NULL
+CREATE TABLE monitoring_schema.schedule (
+  id                  INT PRIMARY KEY NOT NULL,
+  interval_in_seconds INT             NOT NULL,
+  query_id            INT REFERENCES monitoring_schema.query ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE monitoring_schema.condition_sign (
+  id             INT PRIMARY KEY NOT NULL,
+  condition_sign VARCHAR(5)      NOT NULL UNIQUE
 );
 
 
-CREATE TABLE monitoring_schema.schedule(
-   id INT PRIMARY KEY     NOT NULL,
-   interval_in_seconds  int NOT NULL,
-   query_id int REFERENCES monitoring_schema.query ON DELETE CASCADE ON UPDATE CASCADE
+INSERT INTO monitoring_schema.condition_sign (id, condition_sign) VALUES
+  (1, '<'),
+  (2, '<='),
+  (3, '>='),
+  (4, '>'),
+  (5, 'like');
+
+
+CREATE TABLE monitoring_schema.condition (
+  id                INT PRIMARY KEY NOT NULL,
+  condition_sign_id INT REFERENCES monitoring_schema.condition_sign ON DELETE CASCADE ON UPDATE CASCADE,
+  condition_value        VARCHAR(200)    NOT NULL,
+  query_id          INT REFERENCES monitoring_schema.query ON DELETE CASCADE ON UPDATE CASCADE,
+  alert_text        VARCHAR(200)    NOT NULL
 );
 
 
-CREATE TABLE monitoring_schema.condition(
-   id INT PRIMARY KEY     NOT NULL,
-   condition_type varchar(50)  NOT NULL,
-   query_id int REFERENCES monitoring_schema.query ON DELETE CASCADE ON UPDATE CASCADE,
-   alert_text varchar(200)  NOT NULL
-);
-
-CREATE TABLE monitoring_schema.user(
-   id INT PRIMARY KEY     NOT NULL,
-   nick varchar(50)  NOT NULL,
-   first_name varchar(50)  NOT NULL,
-   last_name varchar(100)  NOT NULL
+CREATE TABLE monitoring_schema.user (
+  id         INT PRIMARY KEY NOT NULL,
+  nick       VARCHAR(50)     NOT NULL,
+  first_name VARCHAR(50)     NOT NULL,
+  last_name  VARCHAR(100)    NOT NULL
 );
